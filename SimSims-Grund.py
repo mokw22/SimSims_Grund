cec92c6b075b77f7824ebe277229a271c46c205e
Mohammed Kweder <messi.kweder01@gmail.com>
2023-10-02 17:38:33 +0200

    add a class Lada representing a place there food saves. Food uses as a form of queue(FIFO)

--- SimSims-Grund.py
+++ SimSims-Grund.py
@@ -48,16 +48,42 @@ class Barack:
         if len(self._workers > 0):
             return self._workers.popleft()
         else:
-            raise IndexError("Connot dequeue from an empty queue!")
+            raise IndexError("Connot dequeue a worker from an empty queue!")
 
     def front_worker(self):
         if len(self._workers > 0):
             return self._workers[0]
         else:
-            raise IndexError("Empty queue!")
+            raise IndexError("Empty workers queue!")
 
     def __len__(self):
         return len(self._workers)
 
     def __str__(self):
         return str(self._workers)
+
+
+class Lada:
+    def __init__(self):
+        self._food = deque()
+
+    def recieve_food(self, food):
+        self._food.append(food)
+
+    def send_food(self):
+        if len(self._food > 0):
+            return self._food.popleft()
+        else:
+            raise IndexError("Connot dequeu a food from an empty queue!")
+
+    def front_food(self):
+        if len(self._food > 0):
+            return self.front_food[0]
+        else:
+            raise IndexError("Empty food queue!")
+
+    def __len__(self):
+        return len(self._food)
+
+    def __str__(self):
+        return str(self._food)
